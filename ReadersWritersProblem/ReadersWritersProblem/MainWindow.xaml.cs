d0024e82b42cc8191e77e0abf511d2610d770dd1
Manuel Groß <m.gross@HTBLALeonding.onmicrosoft.com>
2024-02-16 08:30:25 +0100

    changes

--- ReadersWritersProblem/ReadersWritersProblem/MainWindow.xaml.cs
+++ ReadersWritersProblem/ReadersWritersProblem/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Threading;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -23,6 +24,60 @@ namespace ReadersWritersProblem
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private readonly object _syncRoot = new object();
+        private readonly object _syncWrite = new object();
+        private int _rcount = 0;
+        private int _wcount = 0;
+        public void EnterReadLock()
+        {
+            lock (_syncRoot)
+            {
+                while (_wcount > 0)
+                {
+                    Monitor.Wait(_syncRoot);
+                }
+
+                _rcount++;
+            }
+        }
+
+        public void EnterWriteLock()
+        {
+            lock (_syncRoot)
+            {
+                _wcount++;
+
+                while (_rcount > 0)
+                {
+                    Monitor.Wait(_syncRoot);
+                }
+            }
+
+            Monitor.Enter(_syncWrite);
+        }
+        public void ExitReadLock()
+        {
+            lock (_syncRoot)
+            {
+                if (--_rcount == 0 && _wcount > 0)
+                {
+                    Monitor.PulseAll(_syncRoot);
+                }
+            }
+        }
+        
+        public void ExitWriteLock()
+        {
+            Monitor.Exit(_syncWrite);
+        
+            lock (_syncRoot)
+            {
+                if (--_wcount == 0)
+                {
+                    Monitor.PulseAll(_syncRoot);
+                }
+            }
         }
     }
 }
--- ReadersWritersProblem/ReadersWritersProblem/reader.cs
+++
@@ -1,6 +0,0 @@
-namespace ReadersWritersProblem;
-
-public class reader
-{
-    
-}
